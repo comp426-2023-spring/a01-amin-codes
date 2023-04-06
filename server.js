e4963ce8d5cceaba0b9f361ff6b89a3de4dbe24e
amin-codes <monkey62016@hotmail.com>
2023-04-06 19:50:45 -0400

    add server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var argv = minimist(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = argv.port || 3000;
-
+var text = fs.readFileSync("./public/index.html", 'utf8');
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
@@ -22,12 +22,8 @@ const port = argv.port || 3000;
 const server = http.createServer(function (req, res) {
       console.log("Server listening on port ${port}");
       res.writeHead(200, {'Content-Type': 'text/html'});
-      fs.readFile('./public/index.html', (error, data) => {
-            if (error) {
-                console.err(error);
-            }
-            res.end(data);
-      });
+      res.write(text);
+      res.end();
 }).listen(port);
 
 // If there is an error, put it on the console error and return. 
